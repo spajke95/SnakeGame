4cde384ed19696cf2288c39d883ae1b861cdf914
spajke95 <caa995@gmail.com>
2022-11-23 17:36:35 +0100

    Add files via upload

---
+++ GameFrame.java
@@ -0,0 +1,25 @@
+
+import javax.swing.JFrame;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author aca
+ */
+public class GameFrame extends JFrame {
+    GameFrame(){
+    GamePanel panel=new GamePanel();
+    this.add(panel);
+    this.setTitle("Snake");
+    this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    this.setResizable(false);
+    this.pack();
+    this.setVisible(true);
+    this.setLocationRelativeTo(null);
+    
+    };
+}
---
+++ Snake.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author aca
+ */
+public class Snake {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        GameFrame frame=new GameFrame();
+
+// TODO code application logic here
+    }
+    
+}
